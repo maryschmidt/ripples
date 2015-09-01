98d2bc7b41cd2585104ba582aa70040dfaf84417
Clay Gimenez <claygimenez@gmail.com>
2015-08-31 20:33:28 -0400

    sunburst wired up

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,5 +8,11 @@ class WelcomeController < ApplicationController
   def explore
     @clusters_511_all = File.read(File.absolute_path("lib/assets/comparisons/clusters_511_all.json")).html_safe
     @clusters_514_all = File.read(File.absolute_path("lib/assets/comparisons/clusters_514_all.json")).html_safe
+    @clusters_511_na = File.read(File.absolute_path("lib/assets/clusters/NA_RANKED_5.11.json")).html_safe
+    @clusters_514_na = File.read(File.absolute_path("lib/assets/clusters/NA_RANKED_5.14.json")).html_safe
+    @clusters_511_euw = File.read(File.absolute_path("lib/assets/clusters/EUW_RANKED_5.11.json")).html_safe
+    @clusters_514_euw = File.read(File.absolute_path("lib/assets/clusters/EUW_RANKED_5.14.json")).html_safe
+    @clusters_511_kr = File.read(File.absolute_path("lib/assets/clusters/KR_RANKED_5.11.json")).html_safe
+    @clusters_514_kr = File.read(File.absolute_path("lib/assets/clusters/KR_RANKED_5.14.json")).html_safe
   end
 end
