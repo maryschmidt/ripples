39a073c2a6584fcd61a5394f682e93c1206e1c4f
Mary Schmidt <marykateschmidt@gmail.com>
2015-08-31 22:23:00 -0400

    Still more of these

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   config.middleware.use Rack::Deflater
 
   # Ensure requests are only served from one, canonical host name
-  config.middleware.use Rack::CanonicalHost, ENV.fetch("HOST")
+  # config.middleware.use Rack::CanonicalHost, ENV.fetch("HOST")
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = ENV.fetch("ASSET_HOST", ENV.fetch("HOST"))
+  # config.action_controller.asset_host = ENV.fetch("ASSET_HOST", ENV.fetch("HOST"))
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
